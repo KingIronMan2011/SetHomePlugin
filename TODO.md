b281f47128b06264849cece58e679d618e30b629
Julian Speckmann <176780813+KingIronMan2011@users.noreply.github.com>
2025-08-23 00:26:52 +0200

    Added more clear error messages

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# TODO
+
+- [ ] Improve error messages and user feedback
+- [ ] Add configurable maximum number of homes per player
+- [ ] Add support for teleport cooldown/warmup cancellation on player movement
+- [ ] Refactor code for better separation of concerns
+- [ ] Add localization/multi-language support
+- [ ] Document all public methods and classes
+- [ ] Add support for MySQL or other database storage (optional)
--- src/main/java/com/kingironman/sethome/utilities/MessageUtils.java
+++ src/main/java/com/kingironman/sethome/utilities/MessageUtils.java
@@ -21,32 +21,42 @@ public class MessageUtils {
     }
 
     public void displayMessage(CommandSender sender, MESSAGE_TYPE messageType, Integer seconds) {
-        if (messageType == MESSAGE_TYPE.CMD_SETHOME) {
-            sender.sendMessage(formatMessage(SetHome.getInstance().configUtils.MESSAGE_CMD_SETHOME, sender.getName(), Optional.ofNullable(seconds)));
+        String playerName = sender.getName();
+        Optional<Integer> secOpt = Optional.ofNullable(seconds);
+        String msg = null;
+        switch (messageType) {
+            case CMD_SETHOME:
+                msg = SetHome.getInstance().configUtils.MESSAGE_CMD_SETHOME;
+                break;
+            case CMD_HOME:
+                msg = SetHome.getInstance().configUtils.MESSAGE_CMD_HOME;
+                break;
+            case CMD_DELETEHOME:
+                msg = SetHome.getInstance().configUtils.MESSAGE_CMD_DELETEHOME;
+                break;
+            case MISSING_HOME:
+                msg = SetHome.getInstance().configUtils.MESSAGE_MISSING_HOME;
+                break;
+            case MISSING_WORLD:
+                msg = SetHome.getInstance().configUtils.MESSAGE_MISSING_WORLD;
+                break;
+            case COOLDOWN:
+                msg = SetHome.getInstance().configUtils.MESSAGE_COOLDOWN;
+                break;
+            case WARMUP:
+                msg = SetHome.getInstance().configUtils.MESSAGE_WARMUP;
+                break;
+            case ON_MOVE:
+                msg = SetHome.getInstance().configUtils.MESSAGE_ON_MOVE;
+                break;
+            case DENY_CONSOLE:
+                msg = SetHome.getInstance().configUtils.MESSAGE_DENY_CONSOLE;
+                break;
         }
-        else if (messageType == MESSAGE_TYPE.CMD_HOME) {
-            sender.sendMessage(formatMessage(SetHome.getInstance().configUtils.MESSAGE_CMD_HOME, sender.getName(), Optional.ofNullable(seconds)));
-        }
-        else if (messageType == MESSAGE_TYPE.CMD_DELETEHOME) {
-            sender.sendMessage(formatMessage(SetHome.getInstance().configUtils.MESSAGE_CMD_DELETEHOME, sender.getName(), Optional.ofNullable(seconds)));
-        }
-        else if (messageType == MESSAGE_TYPE.MISSING_HOME) {
-            sender.sendMessage(formatMessage(SetHome.getInstance().configUtils.MESSAGE_MISSING_HOME, sender.getName(), Optional.ofNullable(seconds)));
-        }
-        else if (messageType == MESSAGE_TYPE.MISSING_WORLD) {
-            sender.sendMessage(formatMessage(SetHome.getInstance().configUtils.MESSAGE_MISSING_WORLD, sender.getName(), Optional.ofNullable(seconds)));
-        }
-        else if (messageType == MESSAGE_TYPE.COOLDOWN) {
-            sender.sendMessage(formatMessage(SetHome.getInstance().configUtils.MESSAGE_COOLDOWN, sender.getName(), Optional.ofNullable(seconds)));
-        }
-        else if (messageType == MESSAGE_TYPE.WARMUP) {
-            sender.sendMessage(formatMessage(SetHome.getInstance().configUtils.MESSAGE_WARMUP, sender.getName(), Optional.ofNullable(seconds)));
-        }
-        else if (messageType == MESSAGE_TYPE.ON_MOVE) {
-            sender.sendMessage(formatMessage(SetHome.getInstance().configUtils.MESSAGE_ON_MOVE, sender.getName(), Optional.ofNullable(seconds)));
-        }
-        else if (messageType == MESSAGE_TYPE.DENY_CONSOLE) {
-            sender.sendMessage(formatMessage(SetHome.getInstance().configUtils.MESSAGE_DENY_CONSOLE, sender.getName(), Optional.ofNullable(seconds)));
+        if (msg != null) {
+            sender.sendMessage(formatMessage(msg, playerName, secOpt));
+        } else {
+            sender.sendMessage(ChatColor.RED + "[SetHome] An unknown error occurred. Please contact an admin.");
         }
     }
 
